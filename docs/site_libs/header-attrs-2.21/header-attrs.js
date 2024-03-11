06de6f53310a6d477d24ab5bac0c014fed460a9c
ffrings99 <faithfrings@gmail.com>
2024-03-11 11:49:55 -0400

    updated schedule

--- docs/site_libs/header-attrs-2.21/header-attrs.js
+++ docs/site_libs/header-attrs-2.21/header-attrs.js
@@ -2,10 +2,11 @@
 // be compatible with the behavior of Pandoc < 2.8).
 document.addEventListener('DOMContentLoaded', function(e) {
   var hs = document.querySelectorAll("div.section[class*='level'] > :first-child");
-  var i, h;
+  var i, h, a;
   for (i = 0; i < hs.length; i++) {
     h = hs[i];
     if (!/^h[1-6]$/i.test(h.tagName)) continue;  // it should be a header h1-h6
-    h.style.color = "black";  // Set the color of the header to black
+    a = h.attributes;
+    while (a.length > 0) h.removeAttribute(a[0].name);
   }
 });
